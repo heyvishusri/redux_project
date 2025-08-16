592a2b71af7494ce5ee99a3c670d7914e166fb0f
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-17 00:36:51 +0530

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Counter from "./features/counter/Counter";
 export default function App() {
   return (
     <main style={{ fontFamily: "system-ui, sans-serif", padding: 24 }}>
-      <h1>Redux Toolkit + Vite (Beginner)</h1>
+      <h1>Counter App using redux </h1>
       <Counter />
     </main>
   );
